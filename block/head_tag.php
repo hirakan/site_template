034ff7c5424ea4e46a3a99476d69653749a999db
hiramatsu <hiramatsu@cueplus.com>
2017-04-03 11:27:24 +0900

    ドロワー修正

--- block/head_tag.php
+++ block/head_tag.php
@@ -1,8 +1,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="shortcut icon" href="/img/share/favicon.ico" />
+  <link href="/css/drawer.css" rel="stylesheet">
+
 <link href="/css/style.css" rel="stylesheet" type="text/css" media="all" />
 <script src="/js/jquery-1.8.1.min.js" type="text/javascript" charset="utf-8"></script> 
+  <script src="/js/iScroll.js"></script>
+ <script src="/js/drawer.js" charset="utf-8"></script>
 <script src="/js/ie-css3.js" type="text/javascript" charset="utf-8"></script> 
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
